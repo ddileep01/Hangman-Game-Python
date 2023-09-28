8af966bd1e51f1759ff839463fdb7d1d43c4984a
ddileep01 <dileep18052001@gmail.com>
2023-09-28 21:47:10 +0530

    feat: added ASCII art and improved UI

--- main.py
+++ main.py
@@ -1,70 +1,17 @@
+import os
+clear = lambda: os.system('cls')
 import random
 
-stages = ['''
-  +---+
-  |   |
-  O   |
- /|\  |
- / \  |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
- /    |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|   |
-      |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
-  |   |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
-      |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-      |
-      |
-      |
-      |
-=========
-''']
+from hangman_words import word_list
 
-end_of_game = False
-word_list = ["ardvark", "baboon", "camel"]
 chosen_word = random.choice(word_list)
 word_length = len(chosen_word)
 
+end_of_game = False
 lives = 6
 
-print(f'Pssst, the solution is {chosen_word}.')
+from hangman_art import logo
+print(logo)
 
 display = []
 for _ in range(word_length):
@@ -72,6 +19,10 @@ for _ in range(word_length):
 
 while not end_of_game:
     guess = input("Guess a letter: ").lower()
+    clear()
+
+    if guess in display:
+        print(f"You've already guessed {guess}")
 
     for position in range(word_length):
         letter = chosen_word[position]
@@ -80,6 +31,8 @@ while not end_of_game:
             display[position] = letter
 
     if guess not in chosen_word:
+        print(f"You guessed {guess}, that's not in the word. You lose a life.")
+        
         lives -= 1
         if lives == 0:
             end_of_game = True
@@ -91,4 +44,5 @@ while not end_of_game:
         end_of_game = True
         print("You win.")
 
+    from hangman_art import stages
     print(stages[lives])
