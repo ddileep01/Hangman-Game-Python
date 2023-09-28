49b7e8e766473eea7f5c6e8e5d82d67df19260e2
ddileep01 <dileep18052001@gmail.com>
2023-09-28 21:36:16 +0530

    feat: replacing blanks with guesses

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
-word_list = ["aardvark", "baboon", "camel"]
 import random
-
+word_list = ["aardvark", "baboon", "camel"]
 chosen_word = random.choice(word_list)
+
+print(f'Pssst, the solution is {chosen_word}.')
+
+display = []
+word_length = len(chosen_word)
+for _ in range(word_length):
+    display += "_"
 
 guess = input("Guess a letter: ").lower()
 
-for letter in chosen_word:
+for position in range(word_length):
+    letter = chosen_word[position]
+
     if letter == guess:
-        print("Right")
-    else:
-        print("Wrong")
+        display[position] = letter
+
+print(display)
