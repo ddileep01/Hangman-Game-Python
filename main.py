82421ce92d3cfab73fd4a870a6656ae7022ba62e
ddileep01 <dileep18052001@gmail.com>
2023-09-28 21:39:14 +0530

    feat: checking won or lose

--- main.py
+++ main.py
@@ -1,20 +1,27 @@
 import random
 word_list = ["aardvark", "baboon", "camel"]
 chosen_word = random.choice(word_list)
+word_length = len(chosen_word)
 
 print(f'Pssst, the solution is {chosen_word}.')
 
 display = []
-word_length = len(chosen_word)
 for _ in range(word_length):
     display += "_"
 
-guess = input("Guess a letter: ").lower()
+end_of_game = False
 
-for position in range(word_length):
-    letter = chosen_word[position]
+while not end_of_game:
+    guess = input("Guess a letter: ").lower()
 
-    if letter == guess:
-        display[position] = letter
+    for position in range(word_length):
+        letter = chosen_word[position]
+        
+        if letter == guess:
+            display[position] = letter
 
-print(display)
+    print(display)
+
+    if "_" not in display:
+        end_of_game = True
+        print("You win.")
